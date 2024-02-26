015f76c29d045069b48659f77af8da18510d3bd9
Artem <nyphloulzqqq1@gmail.com>
2024-02-26 15:30:26 +0300

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Артем Аверьянов on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
