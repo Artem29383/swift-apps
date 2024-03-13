9a996a8ecf610b89f6b8ecbaf8ad00da7dc551f3
Artem <nyphloulzqqq1@gmail.com>
2024-03-13 14:29:05 +0300

    add some new features to the tests app

--- todolist/InvestApp/api/Handlers.swift
+++ todolist/InvestApp/api/Handlers.swift
@@ -4,82 +4,12 @@
 ////
 ////  Created by Артем Аверьянов on 05.03.2024.
 ////
-//
-//import Foundation
-//
-//class Handlers {
-//    static let API_NWTN = "https://alpha.newton-technology.ru/api/2.95.0/"
-//    static let API_SNP = "https://interns-test-fe.snp.agency/api/v1/"
-//    static let scope = "crfcksoqlllalaooasodoasodasdasxmxmmakk1123123"
-//    
-//    static var session: URLSession = {
-//        let sessionConfig = URLSessionConfiguration.default
-//        // Включаем передачу кук в запросах
-//        sessionConfig.httpShouldSetCookies = true
-//        // Используем общее хранилище кук
-//        sessionConfig.httpCookieStorage = HTTPCookieStorage.shared
-//        return URLSession(configuration: sessionConfig)
-//    }()
-//    
-//    
-//    static private func handleResponse(data: Data?, response: URLResponse?, error: Error?, completion: @escaping (Result<Data, Error>) -> Void) {
-//        if let error = error {
-//            completion(.failure(error))
-//            return
-//        }
-//        
-//        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-//            let statusCode = (response as? HTTPURLResponse)?.statusCode ?? -1
-//            completion(.failure(NSError(domain: "HTTPError", code: statusCode, userInfo: nil)))
-//            return
-//        }
-//        
-//        if let data = data {
-//            completion(.success(data))
-//        } else {
-//            completion(.failure(NSError(domain: "DataError", code: -1, userInfo: nil)))
-//        }
-//    }
-//    
-//    static func getRequest(url: URL, scopeKey: String?, completion: @escaping (Result<Data, Error>) -> Void) {
-//        var request = URLRequest(url: url)
-//        if let scopeKey = scopeKey {
-//            print("scopeKey", scopeKey)
-//            request.addValue(scopeKey, forHTTPHeaderField: "scope-key")
-//        }
-//        
-//        let task = session.dataTask(with: request) { data, response, error in
-//            handleResponse(data: data, response: response, error: error, completion: completion)
-//        }
-//        
-//        task.resume()
-//    }
-//    
-//    static func postRequest(url: URL, scopeKey: String?, body: Data?, completion: @escaping (Result<Data, Error>) -> Void) {
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "POST"
-//        
-//        if let scopeKey = scopeKey {
-//            request.addValue(scopeKey, forHTTPHeaderField: "scope-key")
-//        }
-//        
-//        request.httpBody = body
-//        
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("application/json", forHTTPHeaderField: "Accept")
-//                
-//        let task = session.dataTask(with: request) { data, response, error in
-//            handleResponse(data: data, response: response, error: error, completion: completion)
-//        }
-//        
-//        task.resume()
-//    }
-//}
-//
+
 import Foundation
 
 class Handlers {
-    static let API_NWTN = "https://alpha.newton-technology.ru/api/2.95.0/"
+    //ne2wto4n-te3chn4olo5gy
+    static let API_NWTN = "https://alpha.temp.ru/api/2.95.0/"
     static let API_SNP = "https://interns-test-fe.snp.agency/api/v1/"
     static let scope = "crfcksoqlllalaooasodoasodasdasxmxmmakk1123123"
     
